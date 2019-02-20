95e7f2b9ec2b8e61d669bef1e7903c2579056b35
Cory House <housecor@gmail.com>
2019-02-20 13:26:54 -0600

    Update mockData.js

--- dev-environment/tools/mockData.js
+++ dev-environment/tools/mockData.js
@@ -1,36 +1,92 @@
-const users = [
+const courses = [
   {
     id: 1,
-    name: "Joel",
-    email: "joel@mcd.com"
+    title: "Securing React Apps with Auth0",
+    slug: "react-auth0-authentication-security",
+    authorId: 1,
+    category: "JavaScript"
   },
   {
     id: 2,
-    name: "Cory",
-    email: "cory@reactjsconsulting.com"
-  }
-];
-
-const accounts = [
-  {
-    id: 1,
-    number: 38283,
-    balance: 340.42,
-    userId: 1
+    title: "React: The Big Picture",
+    slug: "react-big-picture",
+    authorId: 1,
+    category: "JavaScript"
   },
   {
-    id: 2,
-    number: 93832,
-    balance: 140.42,
-    userId: 2
+    id: 3,
+    title: "Creating Reusable React Components",
+    slug: "react-creating-reusable-components",
+    authorId: 1,
+    category: "JavaScript"
+  },
+  {
+    id: 4,
+    title: "Building a JavaScript Development Environment",
+    slug: "javascript-development-environment",
+    authorId: 1,
+    category: "JavaScript"
+  },
+  {
+    id: 5,
+    title: "Building Applications with React and Redux",
+    slug: "react-redux-react-router-es6",
+    authorId: 1,
+    category: "JavaScript"
+  },
+  {
+    id: 6,
+    title: "Building Applications in React and Flux",
+    slug: "react-flux-building-applications",
+    authorId: 1,
+    category: "JavaScript"
+  },
+  {
+    id: 7,
+    title: "Clean Code: Writing Code for Humans",
+    slug: "writing-clean-code-humans",
+    authorId: 1,
+    category: "Software Practices"
+  },
+  {
+    id: 8,
+    title: "Architecting Applications for the Real World",
+    slug: "architecting-applications-dotnet",
+    authorId: 1,
+    category: "Software Architecture"
+  },
+  {
+    id: 9,
+    title: "Becoming an Outlier: Reprogramming the Developer Mind",
+    slug: "career-reboot-for-developer-mind",
+    authorId: 1,
+    category: "Career"
+  },
+  {
+    id: 10,
+    title: "Web Component Fundamentals",
+    slug: "web-components-shadow-dom",
+    authorId: 1,
+    category: "HTML5"
   }
 ];
 
-const contact = [];
+const authors = [
+  { id: 1, name: "Cory House" },
+  { id: 2, name: "Scott Allen" },
+  { id: 3, name: "Dan Wahlin" }
+];
+
+const newCourse = {
+  id: null,
+  title: "",
+  authorId: null,
+  category: ""
+};
 
 // Using CommonJS style export so we can consume via Node (without using Babel-node)
 module.exports = {
-  users,
-  accounts,
-  contact
+  newCourse,
+  courses,
+  authors
 };
