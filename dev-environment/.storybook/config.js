5ed2c289f519642e21f6fead0243bf3bec708157
Cory House <housecor@gmail.com>
2019-03-08 11:13:13 -0600

    Update deps

--- dev-environment/.storybook/config.js
+++ dev-environment/.storybook/config.js
@@ -4,22 +4,10 @@ import { withInfo } from "@storybook/addon-info";
 import { withKnobs } from "@storybook/addon-knobs";
 import "storybook-chromatic";
 
-// By convention, automatically load all .stories.js files in src/components in Storybook
-// More: https://storybook.js.org/basics/writing-stories/#loading-stories-dynamically
 const req = require.context("../src/components", true, /\.stories\.js$/);
 
 function loadStories() {
-  req
-    .keys()
-    // Sort by filename. Ignore path and filename casing. This assures the component list is displayed alphabetically in Storybook's navigation.
-    .sort(function(a, b) {
-      const filenameA = a.replace(/^.*[\\\/]/, "").toLowerCase();
-      const filenameB = b.replace(/^.*[\\\/]/, "").toLowerCase();
-      if (filenameA < filenameB) return -1;
-      if (filenameA > filenameB) return 1;
-      return 0;
-    })
-    .forEach(filename => req(filename));
+  req.keys().forEach(filename => req(filename));
 }
 
 // Globally enable addons
